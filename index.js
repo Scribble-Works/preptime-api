f3a84cb7dd22c8c9efa9f0daffb06d26a1e6d644
prince381 <powusu381@gmail.com>
2022-10-27 04:51:23 +0000

    .

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const api = require('./routes/api');
 const download = require('./routes/download');
 
 // const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const DB_HOST = process.env.DB_HOST;
-const DB_PORT = process.env.DB_PORT;
+const DB_PORT = process.env.DB_PORT || 27017;
 const DB_NAME = process.env.DB_NAME;
 
 app.use(express.json())
