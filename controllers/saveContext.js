dd43384660adf1f29c9a115c19c9ba0356d810f7
Antenor <nanaosei452@gmail.com>
2023-05-10 09:43:58 +0000

    try catch for api post

--- controllers/saveContext.js
+++ controllers/saveContext.js
@@ -1,31 +1,34 @@
-const ResponseData = require('../models/responseModel');
-const analyser = require('../functions');
+const ResponseData = require("../models/responseModel");
+const analyser = require("../functions");
 
 module.exports = async (req, res) => {
-    let instance = { ...req.body };
+  let instance = { ...req.body };
+  try {
     // console.log('Attempting to process data:\n',instance)
     instance.questions = analyser.analysisOfAnswers2(
-        instance.dataMatrix,
-        instance.questions,
-        instance.metaData
+      instance.dataMatrix,
+      instance.questions,
+      instance.metaData
     );
     instance.responses = analyser.processDataMatrix(
-        instance.dataMatrix,
-        instance.questions,
-        instance.metaData
+      instance.dataMatrix,
+      instance.questions,
+      instance.metaData
     );
-    instance.metaData.totalQuizScore = analyser.totalQuizScore(instance.questions);
+    instance.metaData.totalQuizScore = analyser.totalQuizScore(
+      instance.questions
+    );
     // instance.dataMatrix = null;
     // console.log(instance)
-    try {
-        const doc = await ResponseData.create(instance)
-        res.status(200).json(doc)
-    } catch(err) {
-        console.log(err.message)
-        res.status(500).json({
-            success: false,
-            message: err.message,
-            data: instance
-        })
-    }
-}
+
+    const doc = await ResponseData.create(instance);
+    res.status(200).json(doc);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      data: instance,
+    });
+  }
+};
