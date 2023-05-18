425978c2483d6b1c045167eafa1aa255a18fbd89
Antenor452 <nanaosei452@gmail.com>
2023-05-18 16:57:21 +0000

    move to hubspot sdk

--- controllers/createContact.js
+++ controllers/createContact.js
@@ -1,27 +1,34 @@
-const axios = require("axios");
-
-const axiosHubspot = axios.create({
-  baseURL: process.env.HUBSPOT_API_BASE_URI,
-  headers: {
-    Authorization: `Bearer ${process.env.HUBSPOT_KEY}`,
-    "Content-Type": "application/json",
-  },
+const hubspot = require("@hubspot/api-client");
+const hubspotClient = new hubspot.Client({
+  accessToken: process.env.HUBSPOT_KEY,
 });
 
 module.exports = async (req, res) => {
   let data = req.body;
+  console.log(data);
   if (!(data.firstname && data.lastname && data.email && data.phone)) {
     return res.status(500).json({
       success: false,
       message: "firstname,lastname,email and phone required",
     });
   }
+
+  const contactObj = {
+    properties: {
+      ...data,
+    },
+  };
   try {
-    const response = await axiosHubspot.post("/crm/v3/objects/contacts", data);
-    res.status(200).json(response.data);
+    const createContactResponse =
+      await hubspotClient.crm.contacts.basicApi.create(contactObj);
+    res.status(200).json({
+      success: true,
+      response: createContactResponse,
+    });
   } catch (e) {
     res.status(500).json({
       success: false,
+      message: e?.body?.message,
     });
   }
 };
